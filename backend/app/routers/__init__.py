aff56dabf2b685de0c66ffa180d4ced6e40e62df
haalkhaz <140852103+haalkhaz@users.noreply.github.com>
2024-07-17 20:09:35 -0500

    Update __init__.py
    updated init file to register posts with the main router blueprint.

--- backend/app/routers/__init__.py
+++ backend/app/routers/__init__.py
@@ -1,5 +1,6 @@
 from .user import user_router
 from .company import company_router
+from .posts import post_router
 from flask import Blueprint
 
 
@@ -10,3 +11,4 @@ router = Blueprint("router", __name__, url_prefix="/api/v1")
 # Registering the different routers
 router.register_blueprint(user_router)
 router.register_blueprint(company_router)
+router.register_blueprint(post_router)
