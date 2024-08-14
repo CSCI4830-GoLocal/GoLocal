5a250b55efd942cd86d09f68182aff8e8d4f84eb
haalkhaz <140852103+haalkhaz@users.noreply.github.com>
2024-08-13 22:24:42 -0500

    Update __init__.py

--- backend/app/routers/__init__.py
+++ backend/app/routers/__init__.py
@@ -2,6 +2,7 @@ from .user import user_router
 from .company import company_router
 from .post import post_router
 from .search import search_router  
+from .reviews import reviews_router
 from flask import Blueprint
 
 # Main Router Blueprint
@@ -12,4 +13,4 @@ router.register_blueprint(user_router)
 router.register_blueprint(company_router)
 router.register_blueprint(post_router)
 router.register_blueprint(search_router)  
-
+router.register_blueprint(reviews_router)
